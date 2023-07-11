0169b3ff35912ce45af18419290a7915dd8171ac
Diego Figueiredo <dmf@cern.ch>
2023-07-11 09:20:41 +0200

    New samples

---
+++ samples_condor_jetsht_2023.sh
@@ -0,0 +1,84 @@
+{
+    "datasets": [
+        {
+            "executable": "/afs/cern.ch/user/d/dmf/private/work/private/CMSPhysicsAnalysis/DarkMatterSearch/2021/NTuplizer/CMSSW_10_6_17_patch1/src/PPSFramework/Analyzer/MissingMassNtupleAnalyzer", 
+            "enable": 1, 
+            "parameters": [
+                "--f $(filename)", 
+                "--year 2018", 
+                "--era A", 
+                "--mode data", 
+                "--physics bjet", 
+                "--jobid $(ProcId)"
+            ], 
+            "inputfiles": "", 
+            "output": "jetsht-data-era-A-year-2018-id0/", 
+            "inputfolder": "/eos/cms/store/group/phys_exotica/PPS-Exo/Grid/jetht-data-2018A-13TEV/crab_dmf_2023-06-30_UTC10-02-46/JetHT/crab_crab_dmf_2023-06-30_UTC10-02-46/230630_080254/0000/", 
+            "id": 0
+        }, 
+        {
+            "executable": "/afs/cern.ch/user/d/dmf/private/work/private/CMSPhysicsAnalysis/DarkMatterSearch/2021/NTuplizer/CMSSW_10_6_17_patch1/src/PPSFramework/Analyzer/MissingMassNtupleAnalyzer", 
+            "enable": 1, 
+            "parameters": [
+                "--f $(filename)", 
+                "--year 2018", 
+                "--era A", 
+                "--mode data", 
+                "--physics bjet", 
+                "--jobid $(ProcId)"
+            ], 
+            "inputfiles": "", 
+            "output": "jetsht-data-era-A-year-2018-id1/", 
+            "inputfolder": "/eos/cms/store/group/phys_exotica/PPS-Exo/Grid/jetht-data-2018A-13TEV/crab_dmf_2023-06-30_UTC10-02-46/JetHT/crab_crab_dmf_2023-06-30_UTC10-02-46/230630_080254/0001/", 
+            "id": 1
+        }, 
+        {
+            "executable": "/afs/cern.ch/user/d/dmf/private/work/private/CMSPhysicsAnalysis/DarkMatterSearch/2021/NTuplizer/CMSSW_10_6_17_patch1/src/PPSFramework/Analyzer/MissingMassNtupleAnalyzer", 
+            "enable": 1, 
+            "parameters": [
+                "--f $(filename)", 
+                "--year 2018", 
+                "--era A", 
+                "--mode data", 
+                "--physics bjet", 
+                "--jobid $(ProcId)"
+            ], 
+            "inputfiles": "", 
+            "output": "jetsht-data-era-A-year-2018-id2/", 
+            "inputfolder": "/eos/cms/store/group/phys_exotica/PPS-Exo/Grid/jetht-data-2018A-13TEV/crab_dmf_2023-06-30_UTC10-02-46/JetHT/crab_crab_dmf_2023-06-30_UTC10-02-46/230630_080254/0002/", 
+            "id": 2
+        }, 
+        {
+            "executable": "/afs/cern.ch/user/d/dmf/private/work/private/CMSPhysicsAnalysis/DarkMatterSearch/2021/NTuplizer/CMSSW_10_6_17_patch1/src/PPSFramework/Analyzer/MissingMassNtupleAnalyzer", 
+            "enable": 1, 
+            "parameters": [
+                "--f $(filename)", 
+                "--year 2018", 
+                "--era B", 
+                "--mode data", 
+                "--physics bjet", 
+                "--jobid $(ProcId)"
+            ], 
+            "inputfiles": "", 
+            "output": "jetsht-data-era-B-year-2018-id3/", 
+            "inputfolder": "/eos/cms/store/group/phys_exotica/PPS-Exo/Grid/jetht-data-2018B-13TEV/crab_dmf_2023-06-30_UTC10-02-54/JetHT/crab_crab_dmf_2023-06-30_UTC10-02-54/230630_080301/0000/", 
+            "id": 3
+        }, 
+        {
+            "executable": "/afs/cern.ch/user/d/dmf/private/work/private/CMSPhysicsAnalysis/DarkMatterSearch/2021/NTuplizer/CMSSW_10_6_17_patch1/src/PPSFramework/Analyzer/MissingMassNtupleAnalyzer", 
+            "enable": 1, 
+            "parameters": [
+                "--f $(filename)", 
+                "--year 2018", 
+                "--era B", 
+                "--mode data", 
+                "--physics bjet", 
+                "--jobid $(ProcId)"
+            ], 
+            "inputfiles": "", 
+            "output": "jetsht-data-era-B-year-2018-id4/", 
+            "inputfolder": "/eos/cms/store/group/phys_exotica/PPS-Exo/Grid/jetht-data-2018B-13TEV/crab_dmf_2023-06-30_UTC10-02-54/JetHT/crab_crab_dmf_2023-06-30_UTC10-02-54/230630_080301/0001/", 
+            "id": 4
+        } 
+    ]
+}
